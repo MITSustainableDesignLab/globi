54bf22daa7f66fae4e0473454ae00ed36ad0989a
Sam Wolk <36545842+szvsw@users.noreply.github.com>
2026-02-19 08:33:06 -0500

    slightly change pathing syntax

--- src/globi/pipelines.py
+++ src/globi/pipelines.py
@@ -262,7 +262,7 @@ def simulate_globi_building_pipeline(
         if spec.parent_experiment_spec.hourly_data_config.does_dataframe_output:
             for meter_name in hourly_df.columns.get_level_values("Meter").unique():
                 variable_df = hourly_df.xs(meter_name, level="Meter", axis=1)
-                dataframe_key = f"HourlyData_{meter_name.replace(' ', '_')}"
+                dataframe_key = f"HourlyData.{meter_name.replace(' ', '')}"
                 dfs[dataframe_key] = variable_df
         if spec.parent_experiment_spec.hourly_data_config.does_file_output:
             hourly_data_outpath = tempdir / "outputs_hourly_data.pq"
